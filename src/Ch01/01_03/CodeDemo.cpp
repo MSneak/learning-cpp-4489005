a117520a73336dc86cfe4cd681d89cfe19fb1157
MSneak <131906454+MSneak@users.noreply.github.com>
2024-07-24 14:43:18 +0000

    completed

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -7,8 +7,9 @@
 
 int main(){
     std::string str;
+    std::cout << "Hi whats your name " << std::flush; 
     std::cin >> str;
-    std::cout << str;
+    std::cout << "glad to meet you, " << str << "!" << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
