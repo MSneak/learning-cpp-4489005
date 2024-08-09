00766526528708757356b0b1c27b81ee22f08410
MSneak <131906454+MSneak@users.noreply.github.com>
2024-08-09 11:57:42 +0000

    continue

--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -5,7 +5,18 @@
 #include <iostream>
 
 int main(){
-    
+    int age [4];
+
+    age [0] = 25;
+    age [1] = 20;
+    age [2] = 19;
+    age [3] = 19;
+
+    std::cout << "age[0] = " << age[0] << std::endl;
+    std::cout << "age[1] = " << age[1] << std::endl;
+    std::cout << "age[2] = " << age[2] << std::endl;
+    std::cout << "age[3] = " << age[3] << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
