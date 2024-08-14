4a52aef7507362d7d66f8680af16495dc911e452
MSneak <131906454+MSneak@users.noreply.github.com>
2024-08-14 09:54:08 +0000

    stage 2 done

--- src/Ch02/02_14/CodeDemo.cpp
+++ src/Ch02/02_14/CodeDemo.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 
 int main(){
-    int nums[5] = {1,23,32,24,337};
+    int nums [5] = {1,23,32,24,337};
     float result; 
 
     result = nums[0] + nums[1] + nums[2] + nums[3] + nums[4];
